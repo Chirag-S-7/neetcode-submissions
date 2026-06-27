3a1b28b92ef40deadcf5e494edb93125e6c22bf7
Chirag Shah <90342267+Chirag-S-7@users.noreply.github.com>
2026-06-27 17:23:27 +0530

    Add: wiggle-sort - submission-0

---
+++ Data Structures & Algorithms/wiggle-sort/submission-0.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun wiggleSort(nums: IntArray) {
+        for (i in 1 until nums.size) {
+            if ((i % 2 == 1 && nums[i] < nums[i - 1]) ||
+                (i % 2 == 0 && nums[i] > nums[i - 1])) {
+                val temp = nums[i]
+                nums[i] = nums[i - 1]
+                nums[i - 1] = temp
+            }
+        }
+    }
+}
