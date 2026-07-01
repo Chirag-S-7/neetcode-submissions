44ef3f2b99c3aea8987a947b582d270c561e5274
Chirag Shah <90342267+Chirag-S-7@users.noreply.github.com>
2026-07-01 16:28:29 +0530

    Add: score-of-a-string - submission-0

---
+++ Data Structures & Algorithms/score-of-a-string/submission-0.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun scoreOfString(s: String): Int {
+        var ans = 0
+        for(i in 1 until s.length){
+            ans+=(abs(s[i]-s[i-1]))
+        }
+        return ans
+    }
+}
